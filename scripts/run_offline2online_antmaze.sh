690e8a5fde7af7934c8f93281ea734757a4f6bd0
nakamotoo <mitsuhiko0820.acm@gmail.com>
2023-01-23 19:11:54 -0800

    fix script

--- scripts/run_offline2online_antmaze.sh
+++ scripts/run_offline2online_antmaze.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 export XLA_PYTHON_CLIENT_PREALLOCATE=False
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=0
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 # export WANDB_DISABLED=True
 
-# env=antmaze-medium-play-v2
+env=antmaze-medium-play-v2
 env=antmaze-medium-diverse-v2
 # env=antmaze-large-play-v2
 # env=antmaze-large-diverse-v2
 
 mixing_ratio=0.5
 # online_expa_max=100000
-online_temperature=100000
+online_temperature=10
 
-for seed in 42 43 44
+# 7 8 9
+for seed in 4 5 6
 do
 python train_finetune.py \
 --env_name=$env \
 --config=configs/antmaze_finetune_config.py \
 --eval_episodes=50 \
---eval_interval=20000 \
+--eval_interval=50000 \
 --replay_buffer_size=2000000 \
 --save_dir=/raid/mitsuhiko/iql/${env}-${mixing_ratio} \
---num_pretraining_steps=500000 \
+--num_pretraining_steps=1000000 \
 --max_steps=1000000 \
 --logging.online \
 --seed $seed \
 --mixing_ratio=$mixing_ratio \
---logging.project=IQL-offline2online-antmaze-temparatre-0117 \
+--logging.project=IQL-offline2online-antmaze-final \
 --online_temperature=$online_temperature \
 --log_interval=5000
 
